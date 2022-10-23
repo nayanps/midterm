b6ec8c3b2e5e5e682a2d659cf33eac2eeb9ca21f
nayanps <nayanpsaha@gmail.com>
2022-10-23 13:02:39 -0400

    added hoverables to desk

---
+++ Assets/scripts/DeskControls.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeskControls : MonoBehaviour
+{ 
+
+    //renderers for sparkles
+    public Renderer re_1;
+    public Renderer re_2;
+
+    //make minigame scene variable!!!
+
+    // sparkles don't show up until player hovers over bed
+    void Start()
+    { 
+        re_1 = GameObject.Find("d_sparkles_1").GetComponent<Renderer>();
+        re_1.enabled = false;
+
+        re_2 = GameObject.Find("d_sparkles_2").GetComponent<Renderer>();
+        re_2.enabled = false;
+
+    }
+    void OnMouseOver()
+    { //player hovering over bed
+
+        re_1.enabled = true;
+        re_2.enabled = true;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //go to minigame, use public variable!
+        }
+    }
+
+    private void OnMouseExit()
+    { //player not hovering over bed
+
+        re_1.enabled = false;
+        re_2.enabled = false;
+
+    }
+}
